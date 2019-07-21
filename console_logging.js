f42b2c09ecef96babcac4d2d72615c68ea7d8ac8
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2019-07-21 10:35:13 +0530

    Logging Pro Tips

---
+++ console_logging.js
@@ -0,0 +1,41 @@
+const foo = { name: 'foo', age: 10 };
+const bar = { name: 'bar', age: 20 };
+const baz = { name: 'baz', age: 30 };
+
+//1. Logging
+console.log(foo);
+console.log(bar);
+console.log(baz);
+//{ name: 'foo', age: 10 }
+//{ name: 'bar', age: 20 }
+//{ name: 'baz', age: 30 }
+
+// Pro Logging
+console.log(foo, bar, baz)
+//{ name: 'foo', age: 10 } { name: 'bar', age: 20 } { name: 'baz', age: 30 }
+
+
+//2. Styling in Console.log
+console.log('%c Hello from Console', 'color: orange; font-weight: bold;')
+//Hello from Console
+
+
+//3. Tabular Data
+console.table([foo, bar, baz])
+/*
+┌─────────┬───────┬─────┐
+│ (index) │ name  │ age │
+├─────────┼───────┼─────┤
+│    0    │ 'foo' │ 10  │
+│    1    │ 'bar' │ 20  │
+│    2    │ 'baz' │ 30  │
+└─────────┴───────┴─────┘
+*/
+
+
+//4. Execution Time 
+console.time('Process')
+let i = 0;
+while (i < 1000000) { i++ }
+console.timeEnd('Process')
+//Process: 4.166ms
