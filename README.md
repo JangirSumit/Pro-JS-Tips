7b8f828fe2e0098db98dc2a45c0bb6d20bae21f6
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2019-07-22 20:57:35 +0530

    let var const diffs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Pro-JS-Tips
 
+## Index
+
 1. [Console Logging](https://github.com/JangirSumit/Pro-JS-Tips/tree/master/Console%20Logging)
--- let-var-cont/let-var-const.js
+++ let-var-cont/let-var-const.js
@@ -9,7 +9,7 @@ console.log(baz);
 //20
 //30
 
-//Redeclare same variables
+//We can not redeclare let and const with the same name. E.g.-
 let foo = 20;
 console.log(foo);
 var bar = 30;
@@ -18,9 +18,9 @@ const baz = 40;
 console.log(baz);
 //Error : Uncaught SyntaxError: Identifier 'foo' has already been declared
 //30
-//Error : Uncaught SyntaxError: Identifier 'foo' has already been declared
+//Error : Uncaught SyntaxError: Identifier 'baz' has already been declared
 
-//Assign some other values
+//We can not reassign the value of const variables. E.g.-
 foo = 20;
 console.log(foo);
 bar = 30;
@@ -31,7 +31,7 @@ console.log(baz);
 //30
 //Uncaught TypeError: Assignment to constant variable.
 
-//Change values of object
+//But we can change values of const object
 const emp = { name: "foo", age: 30 }
 console.log(emp);
 emp.age = 35;
@@ -40,10 +40,104 @@ console.log(emp);
 //Conclusion : 
 //1. We can not redeclare the let and const variables
 //2. We can re assign the value in let but can not in const.
+//3. We can change the values of const objects.
 
 
 //2. Scope
+//scope of var is global and accessible through out the functions E.g.-
+//Example 1: if-else
+function func1() {
+    if (true) {
+        var x = 10;
+    }
+    console.log(x);
+}
 
+func1();
+//10
+
+//Example 2: loops
+function func2() {
+    for (var index = 0; index < 10; index++) {
+    }
+    console.log(index);
+}
+
+func2();
+//10
+
+//let and consts are block scoped. E.g.-
+//Example 1: if-else
+function func1() {
+    if (true) {
+        let x = 10;
+        const y = 20;
+    }
+    console.log(x);
+    console.log(x);
+}
+
+//Example 2: loops
+function func2() {
+    for (let index = 0; index < 10; index++) {
+    }
+    console.log(index);
+}
+
+func2();
+//Error : Uncaught SyntaxError: Identifier 'foo' has already been declared
 
 
 //3. Hoisting
+//All declarations (function, var, let, const and class) are hoisted in JavaScript, 
+//while the var declarations are initialized with undefined, 
+//but let and const declarations remain uninitialized. E.g.-
+//Example 1: var hoisting
+console.log(var_x);
+var var_x = 10;
+//undefined
+
+//because it is hoisted like this-
+var var_x;
+console.log(var_x);
+var_x = 10;
+
+//Example 2: let, const
+console.log(let_x);
+let let_x = 10;
+//ReferenceError: let_x is not defined
+console.log(const_x);
+const const_x = 10;
+//ReferenceError: const_x is not defined
+
+
+//4. Initialization
+//const is required to be initialized at the time declaration E.g.-
+const a;
+a = 10;
+console.log(a);
+//Uncaught SyntaxError: Missing initializer in const declaration
+
+//var and let can be initialized after the declaration
+let a;
+a = 10;
+console.log(a);
+//10
+
+var a;
+a = 10;
+console.log(a);
+//10
+
+
+//5. var variables can be found in windows objects. 
+//Same is not valid for let and const
+var x = 10;
+let y = 20;
+const z = 30;
+console.log(window.x);
+console.log(window.y);
+console.log(window.z);
+//10
+//undefined
+//undefined
